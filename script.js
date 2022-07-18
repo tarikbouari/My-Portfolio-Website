93a6cd67221b4e239980cbcfc98934f136ebf3a6
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-07-18 08:29:53 -0700

    Definded Navigation Script

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 let mobileMenu = document.querySelector('.mobile-menu');
 
-let navLink = document.querySelector('.navlink');
-let header = document.querySelector('.header');
+let navLink = document.querySelector('.nav-link');
+let meow = document.querySelector('#nav-bar');
+let cat = document.querySelector('main');
+let logo = document.querySelector('.logo');
+let burger = document.getElementById('hamburger');
+let Container = document.querySelector('.logo-bar')
+
+ burger.addEventListener('click', () => {
+    meow.style.position='absolute';
+    meow.style.height='100vh';
+    meow.style.zIndex='5';
+    navLink.style.display='flex';
+    navLink.style.flexDirection='column';
+    navLink.style.justifyContent='flex-start';
+    navLink.style.alignItems='space-around';
+    logo.style.display='none';
+    cat.style.display='none';
+    burger.style.display='none';
+    Container.style.justifyContent='flex-start';
+
+    
+
+})
