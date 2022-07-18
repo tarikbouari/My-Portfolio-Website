0924bf4ef201dcbcc39dbd8d728292c7c984d945
Rikstone <tarikbouari@gmail.com>
2022-07-18 18:27:50 +0000

    add click event on menu

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
-let mobileMenu = document.querySelector('.mobile-menu');
 
 let navLink = document.querySelector('.nav-link');
 let navbar = document.querySelector('#nav-bar');
-let cat = document.querySelector('main');
+let mainPage = document.querySelector('main');
 let footerSection = document.querySelector('footer');
 let logo = document.querySelector('.logo');
-let burger = document.getElementById('hamburger');
-let Container = document.querySelector('.logo-bar')
+let menuIcon = document.getElementById('hamburger');
+let logoBar  = document.querySelector('.logo-bar');
+let closeMenu = document.querySelector('.close');
+let navbox = document.querySelector('.right');
 
- burger.addEventListener('click', () => {
+ menuIcon.addEventListener('click', () => {
     navbar.style.position='fixed';
     navbar.style.height='100vh';
     navbar.style.zIndex='5';
+    navbar.style.background='#6070FF';
     navLink.style.display='flex';
     navLink.style.top=0;
     navLink.style.flexDirection='column';
     navLink.style.justifyContent='flex-start';
     navLink.style.alignItems='space-around';
     logo.style.display='none';
-    cat.style.display='none';
-    burger.style.display='none';
-    Container.style.display='block';
-    footerSection.style.display='none';
+    // mainPage.style.display='none';
+    menuIcon.style.display='none';
+    logoBar.style.display='block';
+    // footerSection.style.display='none';
+    closeMenu.style.display='block';
+})
 
-
+closeMenu.addEventListener('click', () =>{
+   
+    navbar.style.height='80px';
+    navbar.style.transition ='3ms all ease';
+    navbar.style.background='#fff';
+    menuIcon.style.display = "Block";
+    navLink.style.display='none';
+    closeMenu.style.display='none';
+    logo.style.display='block';
+    logoBar.style.display='flex';
     
 
 })
