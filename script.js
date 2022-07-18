bdf025fbd926d54cfbd413b97846a3c3840cd2d8
Rikstone <tarikbouari@gmail.com>
2022-07-18 16:19:36 +0000

    update navigation-menu

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 let mobileMenu = document.querySelector('.mobile-menu');
 
 let navLink = document.querySelector('.nav-link');
-let meow = document.querySelector('#nav-bar');
+let navbar = document.querySelector('#nav-bar');
 let cat = document.querySelector('main');
+let footerSection = document.querySelector('footer');
 let logo = document.querySelector('.logo');
 let burger = document.getElementById('hamburger');
 let Container = document.querySelector('.logo-bar')
 
  burger.addEventListener('click', () => {
-    meow.style.position='absolute';
-    meow.style.height='100vh';
-    meow.style.zIndex='5';
+    navbar.style.position='fixed';
+    navbar.style.height='100vh';
+    navbar.style.zIndex='5';
     navLink.style.display='flex';
+    navLink.style.top=0;
     navLink.style.flexDirection='column';
     navLink.style.justifyContent='flex-start';
     navLink.style.alignItems='space-around';
     logo.style.display='none';
     cat.style.display='none';
     burger.style.display='none';
-    Container.style.justifyContent='flex-start';
+    Container.style.display='block';
+    footerSection.style.display='none';
+
 
     
 
