894fa53b300800f9486d639cba903831e85d9a13
Rikstone <tarikbouari@gmail.com>
2022-07-19 00:42:49 +0000

    fix errors

--- script.js
+++ script.js
@@ -10,41 +10,41 @@ let navbox = document.querySelector(".right");
 let navItem = document.querySelectorAll(".nav-item");
 
 menuIcon.addEventListener("click", () => {
-  navbar.style.position = "fixed";
-  navbar.style.height = "100vh";
-  navbar.style.zIndex = "5";
-  navbar.style.background = "#6070FF";
-  navLink.style.display = "flex";
-  navLink.style.top = 0;
-  navLink.style.flexDirection = "column";
-  navLink.style.justifyContent = "flex-start";
-  navLink.style.alignItems = "space-around";
-  logo.style.display = "none";
-  menuIcon.style.display = "none";
-  logoBar.style.display = "block";
-  closeMenu.style.display = "block";
+    navbar.style.position = "fixed";
+    navbar.style.height = "100vh";
+    navbar.style.zIndex = "5";
+    navbar.style.background = "#6070FF";
+    navLink.style.display = "flex";
+    navLink.style.top = 0;
+    navLink.style.flexDirection = "column";
+    navLink.style.justifyContent = "flex-start";
+    navLink.style.alignItems = "space-around";
+    logo.style.display = "none";
+    menuIcon.style.display = "none";
+    logoBar.style.display = "block";
+    closeMenu.style.display = "block";
 });
 
 closeMenu.addEventListener("click", () => {
-  closeAll();
-  location.reload();
+    closeAll();
+    location.reload();
 });
 
 navItem.forEach((item) => {
-  item.addEventListener("click", (event) => {
-    closeAll();
-    location.reload();
-  });
+    item.addEventListener("click", (event) => {
+        closeAll();
+        location.reload();
+    });
 });
 
 function closeAll() {
-  navbar.style.height = "80px";
-  navbar.style.transition = "3ms all ease";
-  navbar.style.background = "#fff";
-  menuIcon.style.display = "block";
-  navLink.style.display = "none";
-  closeMenu.style.display = "none";
-  logo.style.display = "block";
-  logoBar.style.display = "flex";
+    navbar.style.height = "80px";
+    navbar.style.transition = "3ms all ease";
+    navbar.style.background = "#fff";
+    menuIcon.style.display = "block";
+    navLink.style.display = "none";
+    closeMenu.style.display = "none";
+    logo.style.display = "block";
+    logoBar.style.display = "flex";
 }
 
