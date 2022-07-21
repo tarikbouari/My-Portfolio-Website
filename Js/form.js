2a823a32e6772191e8ecc4129c7e8b24e437b510
Rikstone <tarikbouari@gmail.com>
2022-07-21 19:55:51 +0000

    add id to input field

--- Js/form.js
+++ Js/form.js
@@ -1,9 +1,17 @@
+const data = {}; 
+const local = {n}
+
+const username = document.getElementById('name');
+const text = document.getElementById('test');
 const email = document.getElementById('email');
 const form = document.getElementsByClassName('form')[0];
 const errorMessage = document.getElementsByClassName('error-message')[0];
 const warning = document.querySelector('.warning-icon');
 
+
 form.addEventListener('submit', (event) => {
+  const fullName = username.value;
+  const textMessage = text.value;
   const emailValue = email.value;
   event.preventDefault();
   if (emailValue === emailValue.toLowerCase()) {
@@ -16,5 +24,10 @@ form.addEventListener('submit', (event) => {
     emailValue.toLowerCase();
     errorMessage.innerHTML = 'Please enter a valid email address (Lowercase only)';
     warning.style.visibility = 'visible';
+  }
+  if(Object.keys(data).length === 0){
+    localStorage.setItem('user',JSON.stringify(data))
+  } else {
+
   }
 });
