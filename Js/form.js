2c22be1dcb5c40d0c5e962828bb786b2e228310c
melashu <meshu102@outlook.com>
2022-07-21 11:49:50 +0300

    Create form variables

---
+++ Js/form.js
@@ -0,0 +1,12 @@
+const email = document.getElementById("email");
+const form = document.getElementsByClassName('form')[0];
+const errorMessage = document.getElementsByClassName('error-message');
+
+
+form.addEventListener('submit', (event)=>{
+    let emailValue = email.value;
+    event.preventDefault();
+    if (emailValue === emailValue.toLowerCase()) {
+        
+    }
+})
