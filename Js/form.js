ff805be2b95d2e13dd49a673e92716d372776d6a
Rikstone <tarikbouari@gmail.com>
2022-07-21 14:22:34 +0000

    set error message

--- Js/form.js
+++ Js/form.js
@@ -1,17 +1,21 @@
 const email = document.getElementById("email");
 const form = document.getElementsByClassName('form')[0];
 const errorMessage = document.getElementsByClassName('error-message')[0];
-
+const warning = document.querySelector('.warning-icon');
 
 form.addEventListener('submit', (event) =>{
     let emailValue = email.value;
     event.preventDefault();
     if (emailValue === emailValue.toLowerCase()) {
         form.submit();
-        reload();
+        form.reset();
+        warning.style.visibility='hidden';
+        errorMessage.style.visibility='hidden';
     }else{
         form.focus();
         emailValue.toLowerCase();
         errorMessage.innerHTML = "Please enter a valid email address (Lowercase only)";
+        warning.style.visibility='visible';
+
     }
 })
