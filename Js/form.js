0a7b40ac04d3772164e2f95b5090b875d133da9e
melashu <meshu102@outlook.com>
2022-07-21 23:31:49 +0300

    Fetch data from local storag

--- Js/form.js
+++ Js/form.js
@@ -1,45 +1,47 @@
-const data = {}; 
+const data = {};
 const local = {};
 
-const username = document.getElementById('name');
-const text = document.getElementById('test');
-const email = document.getElementById('email');
-const form = document.getElementsByClassName('form')[0];
-const errorMessage = document.getElementsByClassName('error-message')[0];
-const warning = document.querySelector('.warning-icon');
+const username = document.getElementById("name");
+const text = document.getElementById("test");
+const email = document.getElementById("email");
+const form = document.getElementsByClassName("form")[0];
+const errorMessage = document.getElementsByClassName("error-message")[0];
+const warning = document.querySelector(".warning-icon");
 
+if (localStorage.length >= 1) {
+  data = JSON.parse(localStorage.getItem("user"));
+  username.value = data["fullName"];
+  email.value = data["email"];
+  text.value = data["textarea"];
+}
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   const fullName = username.value;
   const textMessage = text.value;
   const emailValue = email.value;
   event.preventDefault();
   if (emailValue === emailValue.toLowerCase()) {
-    local['fullName']= fullName;
-    local['email']= emailValue;
-    local['textarea']= textMessage;
-    if(Object.keys(data).length === 0){
-        localStorage.setItem('user',JSON.stringify(local))
-      } else{ 
-         if ((data['fullName'] === local['fullName']) && (data['email'] === local['email']) &&
-         (data['texarea'] === data['textarea'])) {
-           
-
-         } else {
-            localStorage.setItem('user',JSON.stringify(local))
-         }
-         
-      }
+    local["fullName"] = fullName;
+    local["email"] = emailValue;
+    local["textarea"] = textMessage;
+    if (Object.keys(data).length === 0) {
+      localStorage.setItem("user", JSON.stringify(local));
+    } else {
+      if (!(data["fullName"] === local["fullName"] &&
+        data["email"] === local["email"] &&
+          data["textarea"] === data["textarea"])) {
+        localStorage.setItem("user", JSON.stringify(local));  
+      } 
+    }
     form.submit();
     form.reset();
-    warning.style.visibility = 'hidden';
-    errorMessage.style.visibility = 'hidden';
-    
+    warning.style.visibility = "hidden";
+    errorMessage.style.visibility = "hidden";
   } else {
     form.focus();
     emailValue.toLowerCase();
-    errorMessage.innerHTML = 'Please enter a valid email address (Lowercase only)';
-    warning.style.visibility = 'visible';
+    errorMessage.innerHTML =
+      "Please enter a valid email address (Lowercase only)";
+    warning.style.visibility = "visible";
   }
-  
 });
