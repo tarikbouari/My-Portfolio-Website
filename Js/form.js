f052b3d0c3fabaec35fef8fa113fbf1dc2fa4d1c
Rikstone <tarikbouari@gmail.com>
2022-07-21 13:36:13 +0000

    update validate form

--- Js/form.js
+++ Js/form.js
@@ -1,15 +1,17 @@
 const email = document.getElementById("email");
 const form = document.getElementsByClassName('form')[0];
-const errorMessage = document.getElementsByClassName('error-message');
+const errorMessage = document.getElementsByClassName('error-message')[0];
 
 
-form.addEventListener('submit', (event)=>{
+form.addEventListener('submit', (event) =>{
     let emailValue = email.value;
     event.preventDefault();
     if (emailValue === emailValue.toLowerCase()) {
         form.submit();
+        reload();
     }else{
+        form.focus();
         emailValue.toLowerCase();
-        errorMessage[0].innerHTML = "Please enter a valid email address (Lowercase only)";
+        errorMessage.innerHTML = "Please enter a valid email address (Lowercase only)";
     }
 })
