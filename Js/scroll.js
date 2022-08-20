3de7e8860c545a6bcf1cc6b1434ef9587601d8d3
Rikstone <tarikbouari@gmail.com>
2022-08-20 10:52:43 -0700

    add scroll.js

---
+++ Js/scroll.js
@@ -0,0 +1,11 @@
+
+
+//
+const link = document.querySelector('.nav-links');
+
+link.addEventListener('click', e =>{
+    e.preventDefault();
+   const navLink = e.target;
+   console.log("hello");
+    if(lin)
+})
